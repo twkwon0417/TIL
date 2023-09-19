cb4eb0ba5048ba280ce307bb0f454739a5f098d0
twkwon0417 <91003152+twkwon0417@users.noreply.github.com>
2023-09-19 19:00:56 +0900

    Create chong_gi.cpp

---
+++ chong_gi.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b, _a, _b, visited[100004][100004], ans = -1;
+queue<pair<int, int>> q;
+
+
+int main(){
+    cin >> a >> b >> _a >> _b;
+    
+    q.push_back({0, 0});
+    visited[0][0] = 1;
+    while(q.size()){
+        
+    }
+}
