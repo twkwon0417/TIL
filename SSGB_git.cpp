dd421f2fe43edfc2f084bfe1d6631535d9a06ff0
twkwon0417 <91003152+twkwon0417@users.noreply.github.com>
2023-09-28 14:56:34 +0900

    Update SSGB_git.cpp

--- SSGB_git.cpp
+++ SSGB_git.cpp
@@ -1,81 +1,56 @@
-#include<bits/stdc++.h>
-using namespace std; 
-int n, k, a[14][14], y, x, dir;
-struct Point{
-    int y, x, dir;
-}; 
-vector<int> v[14][14];
-vector<Point> status;
-const int dy[] = {0, 0, -1, 1};
-const int dx[] = {1, -1, 0, 0};  
-void move(int y, int x, int dir, int i){
-    int ny = y + dy[dir];
-    int nx = x + dx[dir];
-    if(ny < 0 || ny >= n || nx < 0 || nx >= n || a[ny][nx] == 2){
-        status[i].dir ^= 1;  
-        ny = y + dy[status[i].dir]; 
-        nx = x + dx[status[i].dir];
-        if (ny < 0 || ny >= n || nx < 0 || nx >= n || a[ny][nx] == 2) return; 
-    }
-	// 한칸이동 로직
-    vector<int> &here_v = v[y][x];
-    vector<int> &next_v = v[ny][nx];
-    auto pos = find(here_v.begin(), here_v.end(), i); 
-	// 뒤집어서 보내기
-    if(a[ny][nx] == 1) reverse(pos, here_v.end()); 
-	for(auto it = pos; it != here_v.end(); it++){
-        next_v.push_back(*it); 
-        status[*it].y = ny; 
-        status[*it].x = nx;  
-    }
-    here_v.erase(pos, here_v.end()); 
-    return;   
-}
-bool isOver(){   
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){ 
-            if(v[i][j].size() >= 4){
-                return 1;  
-            }
-        } 
-    }
-    return 0;
-}
-bool simul(){
-    for(int i = 0; i < status.size(); i++){ 
-        int y = status[i].y;
-        int x = status[i].x;
-        int dir = status[i].dir;
-        move(y, x, dir, i); 
-        if(isOver()) return 1; 
-    } 
-    return 0;
-}
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, ls[24][24], visited[24][24], size = 2, ans;
+pair<int, int> b_shark;
+int dy[4] = {-1, 0, 0, 1};
+int dx[4] = {0, -1, 1, 0};
+
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    cin >> n >> k;
+    cin >> n;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            cin >> a[i][j];
+            cin >> ls[i][j];
+            if(ls[i][j] == 9){
+                b_shark.first = i;
+                b_shark.second = j;
+                ls[i][j] == 0;
+            } 
         }
     }
-    for(int i = 0; i < k; i++){
-        cin >> y >> x >> dir; y--; x--; dir--;
-        v[y][x].push_back(i);
-        status.push_back({y, x, dir});
-    }
-
-    int cnt = 0;
-    bool flag = 0;
-    while(cnt <= 1000){
-        cnt++;
-        if(simul()){
-            flag = 1; 
-			break;
+    while(1){
+        bool flag = false;
+        memset(visited, 0, sizeof(visited));
+        queue<pair<int, int>> q;
+        q.push({b_shark.first, b_shark.second});
+        visited[q.front().first][q.front().second] = 1;
+        while(q.size()){
+            int y = q.front().first;
+            int x = q.front().second;
+            q.pop();
+            for(int i = 0; i < 4; i++){
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                if(ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+                if(visited[ny][nx]) continue;
+                if(ls[ny][nx] > size){
+                    visited[ny][nx]++;
+                    continue;
+                }
+                if(ls[ny][nx] && ls[ny][nx] <= size){
+                    size++;
+                    ls[ny][nx] = 0;
+                    ls[q.front().first][q.front().second] = 0;
+                    b_shark = {ny, nx};
+                    ans += visited[y][x];
+                    flag = true;
+                    break;
+                }
+                q.push({ny, nx});
+                visited[ny][nx] = visited[y][x] + 1;
+            }
         }
+        if(!flag) break;
     }
-    if(flag) cout << cnt << "\n";
-    else cout << -1 << "\n";
-    return 0;
+    cout << ans;
 }
